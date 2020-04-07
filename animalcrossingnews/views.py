4d924cce81caf79d12590817995466b7308afc00
Robynn McCrossan <2483898m@student.gla.ac.uk>
2020-04-07 16:50:30 +0100

    last changes

--- animalcrossingnews/views.py
+++ animalcrossingnews/views.py
@@ -6,6 +6,9 @@ from .models import Story
 
 def index(request):
     latest_heading_list = Heading.objects.order_by('-pub_date')[:5]
+    latest_story_list = Story.objects
     context = {'latest_heading_list': latest_heading_list}
-    return render(request, 'animalcrossingnews/index.html', context)
+    return render(request, 'animalcrossingnews/index.html', context, latest_story_list)
+
+
 
